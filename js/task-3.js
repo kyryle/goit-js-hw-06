efb4e9a17d69f88e8dc4fe02293e45fd6caa16c0
user <kyryle0110@gmail.com>
2026-05-25 18:29:24 +0300

    bad try 1

---
+++ js/task-3.js
@@ -0,0 +1,24 @@
+class StringBuilder {
+  #value 
+  getValue() {
+    return this.#value
+  }
+  padEnd(str) {
+    this.#value.push(str)
+  }
+  padStart(str) {
+    this.#value.unshift(str)
+  }
+  padBoth(str) {
+    this.#value.push(str)
+    this.#value.unshift(str)
+  }
+}
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
